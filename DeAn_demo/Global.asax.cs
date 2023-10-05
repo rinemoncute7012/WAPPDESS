153eb8538bdce04e33a19466387701e43cf0b073
rinemoncute7012 <48089958+rinemoncute7012@users.noreply.github.com>
2023-10-05 12:54:52 +0700

    csdl1

--- DeAn_demo/Global.asax.cs
+++ DeAn_demo/Global.asax.cs
@@ -17,5 +17,9 @@ namespace DeAn_demo
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Session_Start()
+        {
+            Session["UserID"] = "1";
+        }
     }
 }
--- MyClass/Model/Categories.cs
+++ MyClass/Model/Categories.cs
@@ -13,25 +13,35 @@ namespace MyClass.Model
     {
         [Key]
         public int Id {get; set;}
-        [Required]
+        [Required(ErrorMessage = "Tên mặt hàng không được để trống")]
+        [Display(Name = "Tên loại hàng")]
         public string Name { get; set; }
-       
+        [Display(Name= "Tên rút gọn")]
         public string Slug { get; set; }
+        [Display(Name = "Cấp cha")]
         public int? ParentId { get; set; }
+        [Display(Name = "Sắp xếp")]
         public int? Order { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Mô tả không được để trống")]
+        [Display(Name = "Mô tả")]
         public string MetaDesc { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Từ khoá không được để trống")]
+        [Display(Name = "Từ khoá")]
         public string MetaKey { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Người tạo không được để trống")]
+        [Display(Name = "Người tạo")]
         public int CreatedBy { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Ngày tạo không được để trống")]
+        [Display(Name = "Ngày tạo")]
         public DateTime CreatedAt { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Người cập nhật không được để trống")]
+        [Display(Name = "Người cập nhật")]
         public int UpdateBy { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Ngày cập nhật không được để trống")]
+        [Display(Name = "Ngày cập nhật")]
         public DateTime UpdateAt { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Trạng thái không được để trống")]
+        [Display(Name = "Trạng thái")]
         public int Status { get; set; }
 
 
