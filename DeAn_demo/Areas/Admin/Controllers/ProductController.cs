4eed40f31aa641b3f52ce6126e58fa1909cebf29
rinemoncute7012 <48089958+rinemoncute7012@users.noreply.github.com>
2023-11-19 02:25:33 +0700

    ui

--- DeAn_demo/Areas/Admin/Controllers/ProductController.cs
+++ DeAn_demo/Areas/Admin/Controllers/ProductController.cs
@@ -44,22 +44,19 @@ namespace DeAn_demo.Areas.Admin.Controllers
         // GET: Admin/Products/Create
         public ActionResult Create()
         {
-            ViewBag.ListCatID = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
-            ViewBag.ListSupID = new SelectList(suppliersDAO.getList("Index"), "Id", "Name");
+            ViewBag.ListCatID = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");   //categories
+            ViewBag.ListSupID = new SelectList(suppliersDAO.getList("Index"), "Id", "Name"); //supplier
             return View();
         }
 
-
-        // POST: Admin/Products/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Admin/Product/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Products products)
         {
             if (ModelState.IsValid)
             {
-                //xu ly tu dong mot so truong
+                //xu ly tu dong cho mot so truong
                 //---Create At
                 products.CreateAt = DateTime.Now;
                 //---Create By
@@ -71,7 +68,7 @@ namespace DeAn_demo.Areas.Admin.Controllers
                 products.UpdateAt = DateTime.Now;
                 //update by
                 products.UpdateBy = Convert.ToInt32(Session["UserID"]);
-                //xu ly cho phan upload hình ảnh
+                //xu ly hinh anh
                 var img = Request.Files["img"];//lay thong tin file
                 if (img.ContentLength != 0)
                 {
@@ -89,17 +86,19 @@ namespace DeAn_demo.Areas.Admin.Controllers
                         img.SaveAs(PathFile);
                     }
                 }//ket thuc phan upload hinh anh
+                 //them moi mau tin vao db
                 productsDAO.Insert(products);
-                //thong bao them moi thanh cong
+
+                //thong bao trang thai thanh cong
                 TempData["message"] = new XMessage("success", "Thêm mới sản phẩm thành công");
                 return RedirectToAction("Index");
             }
-            ViewBag.ListCatID = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
-            ViewBag.ListSupID = new SelectList(suppliersDAO.getList("Index"), "Id", "Name");
+            ViewBag.ListCatID = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");   //categories
+            ViewBag.ListSupID = new SelectList(suppliersDAO.getList("Index"), "Id", "Name"); //supplier
             return View(products);
         }
 
-        // GET: Admin/Products/Edit/5
+        // GET: Admin/Product/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -113,27 +112,26 @@ namespace DeAn_demo.Areas.Admin.Controllers
                 TempData["message"] = new XMessage("danger", "Không tìm thấy sản phẩm");
                 return RedirectToAction("Index");
             }
-            ViewBag.ListCatID = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
-            ViewBag.ListSupID = new SelectList(suppliersDAO.getList("Index"), "Id", "Name");
+            ViewBag.ListCatID = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");   //categories
+            ViewBag.ListSupID = new SelectList(suppliersDAO.getList("Index"), "Id", "Name"); //supplier
             return View(products);
         }
 
-        // POST: Admin/Products/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Admin/Product/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(Products products)
         {
             if (ModelState.IsValid)
             {
-                //xu ly tu dong 1 so truong
+                //xu ly tu dong mot so truong cu the
                 //slug
                 products.Slug = XString.Str_Slug(products.Name);
                 //update at
                 products.UpdateAt = DateTime.Now;
                 //update by
                 products.UpdateBy = Convert.ToInt32(Session["UserID"]);
+                //xu ly hinh anh
                 // Kiểm tra và xóa file hình ảnh cũ
                 string imageName = products.Image;
 
@@ -142,7 +140,7 @@ namespace DeAn_demo.Areas.Admin.Controllers
                 {
                     string slug = XString.Str_Slug(products.Name);
                     string fileExtension = Path.GetExtension(imageName); // Lấy phần mở rộng của tệp tin
-                    string imagePath = Path.Combine(Server.MapPath("~/Public/img/product/"), slug + fileExtension);
+                    string imagePath = Path.Combine(Server.MapPath("~/Public/img/supplier/"), slug + fileExtension);
 
                     if (System.IO.File.Exists(imagePath))
                     {
@@ -170,7 +168,7 @@ namespace DeAn_demo.Areas.Admin.Controllers
                     }
                 }//ket thuc phan upload hinh anh
                  //hien thi thong bao thanh cong
-                TempData["message"] = new XMessage("success", "Cập nhật thông tin thành công");
+                TempData["message"] = new XMessage("success", "Cập nhật sản phẩm thành công");
                 //cap nhat mau tin
                 productsDAO.Update(products);
                 return RedirectToAction("Index");
@@ -179,6 +177,7 @@ namespace DeAn_demo.Areas.Admin.Controllers
         }
 
         // GET: Admin/Products/Delete/5
+        // GET: Admin/Product/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -195,8 +194,7 @@ namespace DeAn_demo.Areas.Admin.Controllers
             return View(products);
         }
 
-
-        // POST: Admin/Products/Delete/5
+        // POST: Admin/Product/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
@@ -217,7 +215,8 @@ namespace DeAn_demo.Areas.Admin.Controllers
                 }
             }
             productsDAO.Delete(products);
-            TempData["message"] = new XMessage("success", "Xóa mẫu tin thành công");
+
+            TempData["message"] = new XMessage("success", "Xóa sản phẩm thành công");
             return RedirectToAction("Trash");
         }
         public ActionResult Status(int? id)
@@ -228,20 +227,20 @@ namespace DeAn_demo.Areas.Admin.Controllers
                 TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
                 return RedirectToAction("Index");
             }
-            Suppliers suppliers = suppliersDAO.getRow(id);
-            if (suppliers == null)
+            Products products = productsDAO.getRow(id);
+            if (products == null)
             {
                 TempData["message"] = new XMessage("danger", "Cập nhật trạng thái thất bại");
                 return RedirectToAction("Index");
             }
             //cap nhat trang thai
-            suppliers.Status = (suppliers.Status == 1) ? 2 : 1;
+            products.Status = (products.Status == 1) ? 2 : 1;
             //cap nhat update at
-            suppliers.UpdateAt = DateTime.Now;
+            products.UpdateAt = DateTime.Now;
             //cap nhat update by
-            suppliers.UpdateBy = Convert.ToInt32(Session["UserID"]);
+            products.UpdateBy = Convert.ToInt32(Session["UserID"]);
             //update db
-            suppliersDAO.Update(suppliers);
+            productsDAO.Update(products);
             //hien thi thong bao
             TempData["message"] = new XMessage("success", "Cập nhật trạng thái thành công");
             //tro ve trang Index
@@ -310,6 +309,5 @@ namespace DeAn_demo.Areas.Admin.Controllers
             //tro ve trang Index
             return RedirectToAction("Trash");
         }
-
     }
 }
